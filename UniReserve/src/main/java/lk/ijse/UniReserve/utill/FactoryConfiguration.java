e81fb09bfec365ced9a575bcaaa426fb4d772d82
Krishan <xp3cto95@gmail.com>
2023-09-04 20:08:41 +0530

    add property configurations

--- UniReserve/src/main/java/lk/ijse/UniReserve/utill/FactoryConfiguration.java
+++ UniReserve/src/main/java/lk/ijse/UniReserve/utill/FactoryConfiguration.java
@@ -1,4 +1,37 @@
 package lk.ijse.UniReserve.utill;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        configuration.setProperties(properties);
+        configuration
+                .addAnnotatedClass(User.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return factoryConfiguration == null ? (factoryConfiguration = new FactoryConfiguration()) : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
